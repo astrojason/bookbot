09e2ac3c0d39d00c5049e1d46e11c2b6f2f1ae33
Jason Sylvester <jason@astrojason.com>
2025-04-28 06:27:06 -0700

    added the report

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-def count_words(text):
-    words = text.split()
-    return len(words)
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+import sys
 
 
-def count_chars(text):
-    char_obj = {}
-    for char in text:
-        char = char.lower()
-        if char in 'abcdefghijklmnopqrstuvwxyz':
-            char_obj[char] = char_obj.get(char, 0) + 1
-    return char_obj
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
-with open('./books/frankenstein.txt') as f:
-    file_contents = f.read()
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 
-print('---Begin report  of books/frankenstein.txt---')
-print(f'{count_words(file_contents)} found in the document')
-print('')
-chars = count_chars(file_contents)
-for char, count in chars.items():
-    print(f"The '{char}'' was found {count} times")
-print('---End report---')
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
+main()
